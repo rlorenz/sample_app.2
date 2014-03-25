79d91b41404184f60abdd14a93c30d4d2d831b61
Randall Lorenz <rlorenz@wisc.edu>
2014-03-25 18:25:03 -0500

    partly through 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+    
   root "static_pages#home"
   get '/help' => 'static_pages#help'
   get '/signup' => 'users#new'
   get '/about' => "static_pages#about"
   get '/contact' => "static_pages#contact"
+    
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
