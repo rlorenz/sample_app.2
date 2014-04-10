2b67c3b5f67ecdd414504bd308bafe0ecc851413
Randall Lorenz <rlorenz@wisc.edu>
2014-04-09 21:19:40 -0700

    Finished signin/out

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,11 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
+    end
         
     it { should be_valid }
     
@@ -49,6 +54,10 @@ describe User do
       end
     end
   end
+    
+    it { should respond_to(:password_confirmation) }
+    it {should respond_to(:remember_token) }
+    it {should respond_to(:authenticate) }
     
     describe "when email address is already taken" do
         before do
